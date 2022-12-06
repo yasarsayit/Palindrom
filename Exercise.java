5d0b732d017856990bf29d3f2f42874c8c64e92c
yasarsayit <90604486+yasarsayit@users.noreply.github.com>
2022-12-06 19:48:43 +0300

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,34 @@
+package java101;
+
+import java.util.Scanner;
+
+public class Exercise {
+
+	public static boolean isPalindrome(int number) {
+
+		int temp = number, lastdigit, reversenumber = 0;
+
+		while (temp != 0) {
+
+			lastdigit = temp % 10;
+			reversenumber = (10 * reversenumber) + lastdigit;
+			temp /= 10;
+
+		}
+		if (reversenumber == number)
+			System.out.print(number + " is palindrome");
+		else
+			System.out.print(number + " is not palindrome");
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number :");
+		int num = scanner.nextInt();
+
+		isPalindrome(num);
+	}
+
+}
